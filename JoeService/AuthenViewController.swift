0950c4fc3a3b6f12abd0c3bcd6b707ed8a08ff9a
Joe <pratchavee.amorn@gmail.com>
2019-02-12 09:53:52 +0700

    Go to Register success

--- JoeService/AuthenViewController.swift
+++ JoeService/AuthenViewController.swift
@@ -14,17 +14,15 @@ class AuthenViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
+    }//viewDidLoad
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    @IBAction func newRegister(_ sender: UIButton) {
+        
+        
+        print("You Click new Register")
+        performSegue(withIdentifier: "GoToRegister", sender: self)
     }
-    */
-
-}
+    
+} // Main Class
